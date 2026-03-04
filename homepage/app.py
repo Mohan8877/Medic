072c4e313c0645de6711b716cfaef6bab3374054
M MOHAN <muneppamohan@gmail.com>
2026-03-04 20:46:53 +0530

    Update app.py

--- homepage/app.py
+++ homepage/app.py
@@ -209,7 +209,7 @@ def handle_message(data):
     db.session.commit()
     emit('receive_message', {'text': data['text'], 'sender': data['sender']}, room=data['room'])
 
-if __name__ == '__main__':
-    with app.app_context(): 
-        db.create_all()
-    socketio.run(app, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
